18c7c861486887cfd8068b92e84077f154f8151e
VadimPetroviOS <107857712+VadimPetroviOS@users.noreply.github.com>
2024-03-21 17:34:55 +0300

    Initial commit

---
+++ InfecSim/InfecSimApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfecSimApp.swift
+//  InfecSim
+//
+//  Created by Вадим on 21.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct InfecSimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
